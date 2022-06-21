652ce78728f8db64cb28ab2a748169380c97eaa0
jonathandkim96 <91635744+jonathandkim96@users.noreply.github.com>
2022-06-21 15:22:04 -0400

    Update unittesting.py

--- Sprint 4/unittesting.py
+++ Sprint 4/unittesting.py
@@ -16,7 +16,19 @@ class functionTest(unittest.TestCase):
         self.assertEqual(sprint4.US26(), result26)
     
     ######################### End of Chaitanya Pawar's Code #########################
-
+    
+    ######################### Jonathan Kim's Code #########################
+    def test_userstory31(self):
+        result31 = ['US31: List of individuals above the age of 30 who have never been married: ',
+                    'Stella /Robinson/',
+                    'Olenna /Tully/']
+        self.assertEqual(sprint4.US31(), result31)
+    
+    def test_userstory32(self):
+        result32 = ['There are no shared birthdays.']
+        self.assertEqual(sprint4.US32(), result32)
+        
+    ######################### End of Jonathan Kim's Code #########################
 
 if __name__ == '__main__':
     unittest.main()
